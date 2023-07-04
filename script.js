092e30ae8aded6bb9c537a036b42fb9e1a3f6a41
Kaio Soledade <soledadekaio@gmail.com>
2023-07-04 13:55:16 -0300

    script revell funcionando

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ sr.reveal ('.we-are-creatives', {durantion: 10000});
 sr.reveal ('.revell', 
     {rotate: {x: 100, y: 0, z: 1000},
     durantion: 2000});
+
+
+
+
+sr.reveal ('.revell-type1', {durantion: 200000});
