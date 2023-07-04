6558ac562864a400efd0b79147f150f42dfc9fff
Kaio Soledade <soledadekaio@gmail.com>
2023-07-03 21:51:07 -0300

    script não funciona ainda

---
+++ script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal ('.we-are-creatives', {durantion: 10000});
+
+sr.reveal ('.revell', 
+    {rotate: {x: 100, y: 0, z: 1000},
+    durantion: 2000});
