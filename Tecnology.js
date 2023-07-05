cd9a45538b3abb9538ed902272fbd1c201642a98
Kaio Soledade <soledadekaio@gmail.com>
2023-07-05 19:08:57 -0300

    add JS de cada HTML

---
+++ Tecnology.js
@@ -0,0 +1,6 @@
+sr.reveal('.revell4', {
+    origin: 'left', // Define a direção da origem da animação
+    distance: '100px', // Define a distância percorrida pela animação
+    duration: 1000, // Define a duração da animação em milissegundos
+    easing: 'ease-in-out', // Define a função de easing (aceleração/desaceleração) da animação
+  });
