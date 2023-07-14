5fc7927028f9ae5e946df483dce04ab7cbef09e6
Kaio Soledade <soledadekaio@gmail.com>
2023-07-14 17:13:06 -0300

    fazendo alteração para resolver o problema

--- Crew.js
+++ Crew.js
@@ -6,51 +6,57 @@ sr.reveal('.revell4', {
     duration: 1000, // Define a duração da animação em milissegundos
     easing: 'ease-in-out', // Define a função de easing (aceleração/desaceleração) da animação
   });
+  sr.reveal('.revell3', {
+    origin: 'left', // Define a direção da origem da animação
+    distance: '100px', // Define a distância percorrida pela animação
+    duration: 2000, // Define a duração da animação em milissegundos
+    easing: 'ease-in-out', // Define a função de easing (aceleração/desaceleração) da animação
+  });
 
 
 
-var CardPlanet1 = document.getElementById('Moon');
-var CardPlanet2 = document.getElementById('Mars');
-var CardPlanet3 = document.getElementById('Europa');
-var CardPlanet4 = document.getElementById('Titan');
+var Person1 = document.getElementById('Douglas');
+var Person2 = document.getElementById('Anousheh');
+var Person3 = document.getElementById('Victor');
+var Person4 = document.getElementById('Mark');
 
 
-var Button1 = document.getElementById('MoB')
+var Button1 = document.querySelectorAll('.DoB')
 Button1.addEventListener("click", submit1);
 function submit1() {
-    CardPlanet1.classList.add('Moon-on');
-    CardPlanet1.classList.remove('Moon-off');
-    CardPlanet2.classList.add('Mars-off');
-    CardPlanet3.classList.add('Europa-off');
-    CardPlanet4.classList.add('Titan-off');
+    Person1.classList.add('Douglas-on');
+    Person1.classList.remove('Douglas-off');
+    Person2.classList.add('Anousheh-off');
+    Person3.classList.add('Victor-off');
+    Person4.classList.add('Mark-off');
 }
 
-var Button2 = document.getElementById('MaB')
+var Button2 = document.querySelectorAll('.AnB')
 Button2.addEventListener("click", submit2);
 function submit2() {
-    CardPlanet2.classList.add('Mars-on');
-    CardPlanet2.classList.remove('Mars-off');
-    CardPlanet1.classList.add('Moon-off');
-    CardPlanet3.classList.add('Europa-off');
-    CardPlanet4.classList.add('Titan-off');
+    Person2.classList.add('Anousheh-on');
+    Person2.classList.remove('Anousheh-off');
+    Person1.classList.add('Douglas-off');
+    Person3.classList.add('Victor-off');
+    Person4.classList.add('Mark-off');
 }
 
-var Button3 = document.getElementById('EuB')
+var Button3 = document.querySelectorAll('.ViB')
 Button3.addEventListener("click", submit3);
 function submit3() {
-    CardPlanet3.classList.add('Europa-on');
-    CardPlanet3.classList.remove('Europa-off');
-    CardPlanet2.classList.add('Mars-off');
-    CardPlanet1.classList.add('Moon-off');
-    CardPlanet4.classList.add('Titan-off');
+    Person3.classList.add('Victor-on');
+    Person3.classList.remove('Victor-off');
+    Person2.classList.add('Anousheh-off');
+    Person1.classList.add('Douglas-off');
+    Person4.classList.add('Mark-off');
 }
 
-var Button4 = document.getElementById('TiB')
+var Button4 = document.querySelectorAll('.MaB')
 Button4.addEventListener("click", submit4);
 function submit4() {
-    CardPlanet4.classList.add('Titan-on');
-    CardPlanet4.classList.remove('Titan-off');
-    CardPlanet2.classList.add('Mars-off');
-    CardPlanet3.classList.add('Europa-off');
-    CardPlanet1.classList.add('Moon-off');
+    Person4.classList.add('Mark-on');
+    Person4.classList.remove('Mark-off');
+    Person2.classList.add('Anousheh-off');
+    Person3.classList.add('Victor-off');
+    Person1.classList.add('Douglas-off');
 }
